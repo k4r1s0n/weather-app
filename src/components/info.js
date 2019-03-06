77be765229728971cf734c79b2e81214d8bab8a0
Alex <ale8under8@gmail.com>
2019-03-06 07:32:53 -0800

    create info.js

---
+++ src/components/info.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Info extends React.Component {
+  render() {
+    return(
+      <div>
+        <h1>Weather Application</h1>
+        <p>Find out weather forecast in your city</p>
+      </div>
+    );
+  }
+}
+export default Info;
